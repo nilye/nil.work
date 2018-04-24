b56bcdd453fe764ebc3df426b395e4d4ebcab20c
entaroalsace <entaroalsace@gmail.com>
2018-04-24 19:51:55 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div id="logo">
+      <h1>NIL</h1>
+      <h1>WO</h1>
+      <h1>RK</h1>
+    </div>
+    <scroller></scroller>
+    <router-view/>
   </div>
 </template>
 
 <script>
+  import Scroll from './components/scroll/scroller.vue'
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  name: 'App',
+  components:{
+    scroller:Scroll
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  @font-face{font-family:Montserrat;font-weight:100;src:local("assets/font/Montserrat-Thin.tff")}
+  @font-face{font-family:Montserrat;font-weight:200;src:local("assets/font/Montserrat-ExtraLight.tff")}
+  @font-face{font-family:Montserrat;font-weight:300;src:local("assets/font/Montserrat-Light.tff")}
+  @font-face{font-family:Montserrat;font-weight:400;src:local("assets/font/Montserrat-Regular.tff")}
+  @font-face{font-family:Montserrat;font-weight:500;src:local("assets/font/Montserrat-Medium.tff")}
+  @font-face{font-family:Montserrat;font-weight:600;src:local("assets/font/Montserrat-SemiBold.tff")}
+  @font-face{font-family:Montserrat;font-weight:700;src:local("assets/font/Montserrat-Bold.tff")}
+  @font-face{font-family:Montserrat;font-weight:800;src:local("assets/font/Montserrat-ExtraBold.tff")}
+  @font-face{font-family:Montserrat;font-weight:900;src:local("assets/font/Montserrat-Black.tff")}
+  body{background:#333;margin:0;-webkit-font-smoothing:antialiased}
+  #logo{top:32px;left:32px;position:absolute}
+  #logo h1{font:600 28px/32px Montserrat;color:#fff;margin:0}
 </style>
---
+++ src/components/scroll/scroller.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="img-cropper" v-for="(item, index) in 5" :style="position(index)">
+      <img :src="image">
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data(){
+      return {
+        image: null,
+        windowHalf:window.innerWidth/2,
+        ovalRadius:150
+      }
+    },
+    created(){
+      window.addEventListener('resize', this.windowOnReize)
+    },
+    methods:{
+      windowOnReize(){
+        this.windowHalf = window.innerWidth/2
+      },
+      position(i){
+        console.log(i)
+        let a = (this.ovalRadius*2)/Math.sqrt(3)-this.ovalRadius
+        console.log(a)
+        if(i % 2 == 0){
+          return {right:this.windowHalf+'px', top: i*(150+a)+'px'}
+        } else {
+          return {left:this.windowHalf+'px', top: i*(150+a)+'px'}
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .img-cropper{position:absolute;width:300px;height:300px;border-radius: 50%;overflow: hidden;background: #fff;}
+  img{width:100%;}
+</style>
